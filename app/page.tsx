4bca4b3ea99a9185ef249d311f5bc7cb5b9b6648
Emanuel Araujo <83252512+emanuelmaraujo@users.noreply.github.com>
2026-09-25 17:35:30 -0300

    fix: use relative component import

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { LandingExperience } from "@/components/landing-experience";
+import { LandingExperience } from "../components/landing-experience";
 
 export default function Home() {
   return <LandingExperience />;
